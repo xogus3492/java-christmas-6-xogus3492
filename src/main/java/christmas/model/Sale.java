0109d9defbda989ef000a5383e14d5e2f772403e
xogus3492 <wkdxogus3492@gmail.com>
2023-11-10 17:02:03 +0900

    Fix: 할인 이름 오타 수정

--- src/main/java/christmas/model/Sale.java
+++ src/main/java/christmas/model/Sale.java
@@ -2,8 +2,8 @@ package christmas.model;
 
 public enum Sale {
     CHRISTMAS_D_DAY_SALE("크리스마스 디데이 할인"),
-    WEEKDAYS_SALE("평일 세일"),
-    WEEKEND_SALE("주말 세일"),
+    WEEKDAYS_SALE("평일 할인"),
+    WEEKEND_SALE("주말 할인"),
     SPECIAL_SALE("특별 할인");
 
     private final String name;
