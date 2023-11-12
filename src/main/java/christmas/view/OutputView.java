bddcf07bfe06590f145b0aa8bfdfba1ae8efad65
xogus3492 <wkdxogus3492@gmail.com>
2023-11-12 16:25:14 +0900

    Feat: 처음 인사 문구 출력 기능

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,10 @@
+package christmas.view;
+
+import christmas.view.message.PrintMessage;
+
+public class OutputView {
+
+    public void printStartMessage() {
+        System.out.println(PrintMessage.START_OUTPUT_MESSAGE.getMessage());
+    }
+}
--- src/main/java/christmas/view/message/PrintMessage.java
+++ src/main/java/christmas/view/message/PrintMessage.java
@@ -1,8 +1,12 @@
 package christmas.view.message;
 
 public enum PrintMessage {
+    // InputView
     VISIT_DATE_INPUT_MESSAGE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
-    ORDER_MENU_INPUT_MESSAGE("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+    ORDER_MENU_INPUT_MESSAGE("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
+
+    // OutputView
+    START_OUTPUT_MESSAGE("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
 
     private final String message;
 
