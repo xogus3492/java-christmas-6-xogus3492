d6d01ccb663312f69e384313c97a88219d2bc22d
xogus3492 <wkdxogus3492@gmail.com>
2023-11-14 13:57:27 +0900

    Test: 주문메뉴에 대한 디저트 포함 여부 테스트

--- src/test/java/christmas/model/OrderTest.java
+++ src/test/java/christmas/model/OrderTest.java
@@ -22,4 +22,19 @@ public class OrderTest {
         //then
         assertThat(totalAmount).isEqualTo(109000);
     }
+
+    @Test
+    void 티본스테이크1개와_초코케이크1개를_주문하면_디저트가_존재한다() {
+        //given
+        Map<String, Integer> orderMenu = new HashMap<>();
+        orderMenu.put("티본스테이크", 1);
+        orderMenu.put("초코케이크", 1);
+        Order order = Order.of(orderMenu);
+
+        //when
+        final boolean existDessert = order.isExistDessert();
+
+        //then
+        assertThat(existDessert).isTrue();
+    }
 }
