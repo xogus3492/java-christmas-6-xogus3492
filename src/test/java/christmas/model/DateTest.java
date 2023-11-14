5b1f9a977115ec4b4b52d5b48e8fa1819d9c9763
xogus3492 <wkdxogus3492@gmail.com>
2023-11-14 14:13:22 +0900

    Test: 날짜에 따른 크리스마스 할인 금액 반환 테스트

--- src/test/java/christmas/model/DateTest.java
+++ src/test/java/christmas/model/DateTest.java
@@ -1,5 +1,6 @@
 package christmas.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
@@ -10,5 +11,18 @@ public class DateTest {
     void 날짜가_1일부터_31일_사이에_포함되지_않으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Date(32))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 날짜가_21일_이라면_추가_2000원_할인_금액을_반환한다() {
+        //given
+        int day = 21;
+        Date date = new Date(day);
+
+        //when
+        final int christmasBenefit = date.getChristmasBenefit();
+
+        //then
+        assertThat(christmasBenefit).isEqualTo(2000);
     }
 }
