bb5e1e52d4cea4a9295d558ecba65d2bb9f1c2af
xogus3492 <wkdxogus3492@gmail.com>
2023-11-14 13:55:45 +0900

    Test: 주문메뉴에 대한 총주문 금액 반환 테스트

---
+++ src/test/java/christmas/model/OrderTest.java
@@ -0,0 +1,25 @@
+package christmas.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class OrderTest {
+
+    @Test
+    void 티본스테이크1개와_바비큐립1개를_주문하면_총주문_금액_109000원을_반환한다() {
+        //given
+        Map<String, Integer> orderMenu = new HashMap<>();
+        orderMenu.put("티본스테이크", 1);
+        orderMenu.put("바비큐립", 1);
+        Order order = Order.of(orderMenu);
+
+        //when
+        final int totalAmount = order.totalOrderAmount();
+
+        //then
+        assertThat(totalAmount).isEqualTo(109000);
+    }
+}
