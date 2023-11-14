1fb70ccfc93c6e4bbcfe3bf67733cd44b4d03bf0
xogus3492 <wkdxogus3492@gmail.com>
2023-11-14 14:23:04 +0900

    Test: 평일 할인 적용 테스트

--- src/test/java/christmas/model/BenefitTest.java
+++ src/test/java/christmas/model/BenefitTest.java
@@ -24,4 +24,23 @@ public class BenefitTest {
         //then
         assertThat(benefit.toString()).contains("크리스마스 디데이 할인: -1,200원");
     }
+
+    @Test
+    void 평일에_디저트2개를_주문하면_평일_할인에_의해_4046원이_할인된다() {
+        //given
+        Date date = new Date(3);
+
+        Map<String, Integer> orderMenu = new HashMap<>();
+        orderMenu.put("초코케이크", 1);
+        orderMenu.put("아이스크림", 1);
+        Order order = Order.of(orderMenu);
+
+        Benefit benefit = new Benefit(date, order);
+
+        //when
+        benefit.applyWeekdaysSale();
+
+        //then
+        assertThat(benefit.toString()).contains("평일 할인: -4,046원");
+    }
 }
