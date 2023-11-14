393e1afd70d2c2da26bca83f687148589cfa63a5
xogus3492 <wkdxogus3492@gmail.com>
2023-11-14 14:36:01 +0900

    Test: 날짜입력이 공백이거나 비어있는 경우에 대한 예외 테스트

---
+++ src/test/java/christmas/util/InputValidatorTest.java
@@ -0,0 +1,20 @@
+package christmas.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class InputValidatorTest {
+
+    @Test
+    void 날짜입력이_공백이거나_비어있으면_예외가_발생하여_false를_반환한다() {
+        //given
+        String input = "";
+
+        //when
+        final boolean exception = InputValidator.verifyVisitDate(input);
+
+        //then
+        assertThat(exception).isFalse();
+    }
+}
