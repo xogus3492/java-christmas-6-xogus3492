91e6cc2294dc741f0e4c34d1b52c10842eda6221
xogus3492 <wkdxogus3492@gmail.com>
2023-11-14 14:49:15 +0900

    Test: 주문메뉴 List 객체로 파싱 테스트

--- src/test/java/christmas/util/InputParserTest.java
+++ src/test/java/christmas/util/InputParserTest.java
@@ -3,6 +3,7 @@ package christmas.util;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.entry;
 
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +20,18 @@ public class InputParserTest {
         //then
         assertThat(orderMenu).hasSize(2)
                 .contains(entry("티본스테이크", 2), entry("초코케이크", 1));
+    }
+
+    @Test
+    void 입력받은_메뉴를_파라미터로_넘기면_각각_메뉴이름을_List_객체로_파싱한다() {
+        //given
+        String input = "바비큐립-1,아이스크림-1,레드와인-1";
+
+        //when
+        final List<String> orderMenu = InputParser.parseMenuList(input);
+
+        //then
+        assertThat(orderMenu).hasSize(3)
+                .contains("바비큐립", "아이스크림", "레드와인");
     }
 }
